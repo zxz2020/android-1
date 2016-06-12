2de904ad6688f453cc9ddcf7f918089d2ab762ac
Bartosz Przybylski <bart.p.pl@gmail.com>
2016-06-12 15:06:37 +0200

    Move enabling/disabling to onResume

--- src/main/java/com/owncloud/android/ui/activity/Preferences.java
+++ src/main/java/com/owncloud/android/ui/activity/Preferences.java
@@ -123,6 +123,7 @@ public class Preferences extends PreferenceActivity
     private Preference mPrefInstantVideoUploadUseSubfolders;
     private Preference mPrefInstantVideoUploadPathWiFi;
     private Preference mPrefInstantVideoUploadOnlyOnCharging;
+    private Preference mPrefTimeBetweenSynchronizations;
     private String mUploadVideoPath;
     private ListPreference mPrefStoragePath;
     private String mStoragePath;
@@ -249,32 +250,22 @@ public class Preferences extends PreferenceActivity
                 return true;
             }
         });
-		final Preference syncGapPreference = findPreference("time_between_sync");
-		if (syncGapPreference != null) {
-            boolean isAutoSyncEnabled = ContentResolver.getSyncAutomatically(AccountUtils.getCurrentOwnCloudAccount(this), getString(R.string.authority));
-            syncGapPreference.setEnabled(isAutoSyncEnabled);
+        mPrefTimeBetweenSynchronizations = findPreference("time_between_sync");
+		if (mPrefTimeBetweenSynchronizations != null) {
 
-            if (isAutoSyncEnabled) {
-                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-                final Resources res = getResources();
-                int minutesBetweenSyncs = Integer.parseInt(prefs.getString("time_between_sync", "60"));
-                syncGapPreference.setSummary(res.getQuantityString(R.plurals.minutes, minutesBetweenSyncs, minutesBetweenSyncs));
-                syncGapPreference.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-                    @Override
-                    public boolean onPreferenceChange(Preference preference, Object newValueObject) {
-                        if (newValueObject instanceof String && ((String) newValueObject).length() > 0) {
-                            int newValue = Integer.parseInt((String) newValueObject);
-                            if (newValue > 0) {
-                                syncGapPreference.setSummary(res.getQuantityString(R.plurals.minutes, newValue, newValue));
-                                return true;
-                            }
+            mPrefTimeBetweenSynchronizations.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValueObject) {
+                    if (newValueObject instanceof String && ((String) newValueObject).length() > 0) {
+                        int newValue = Integer.parseInt((String) newValueObject);
+                        if (newValue > 0) {
+                            mPrefTimeBetweenSynchronizations.setSummary(getResources().getQuantityString(R.plurals.minutes, newValue, newValue));
+                            return true;
                         }
-                        return false;
                     }
-                });
-            } else {
-                syncGapPreference.setSummary(R.string.prefs_time_between_sync_sync_disabled);
-            }
+                    return false;
+                }
+            });
 
 		}
 
@@ -655,6 +646,20 @@ public class Preferences extends PreferenceActivity
                 PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         boolean state = appPrefs.getBoolean(PassCodeActivity.PREFERENCE_SET_PASSCODE, false);
         pCode.setChecked(state);
+
+        boolean isAutoSyncEnabled = ContentResolver.getSyncAutomatically(AccountUtils.getCurrentOwnCloudAccount(this), getString(R.string.authority));
+        mPrefTimeBetweenSynchronizations.setEnabled(isAutoSyncEnabled);
+        String summary;
+        if (isAutoSyncEnabled) {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+            final Resources res = getResources();
+            int minutesBetweenSyncs = Integer.parseInt(prefs.getString("time_between_sync", "60"));
+            summary = res.getQuantityString(R.plurals.minutes, minutesBetweenSyncs, minutesBetweenSyncs);
+        } else {
+            summary = getString(R.string.prefs_time_between_sync_sync_disabled);
+        }
+        mPrefTimeBetweenSynchronizations.setSummary(summary);
+
     }
 
     @Override
